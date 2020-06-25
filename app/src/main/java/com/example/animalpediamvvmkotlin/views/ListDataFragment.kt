320334a180b67b25be5d6f577008d43d25857c9c
RachmanForniandi <rachmanforniandi@gmail.com>
2020-06-25 13:37:09 +0700

    navigation fundamental done.

---
+++ app/src/main/java/com/example/animalpediamvvmkotlin/views/ListDataFragment.kt
@@ -0,0 +1,35 @@
+package com.example.animalpediamvvmkotlin.views
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
+import com.example.animalpediamvvmkotlin.R
+import kotlinx.android.synthetic.main.fragment_list_data.*
+
+
+class ListDataFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_data, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab_1.setOnClickListener {
+            val actionToDetail:NavDirections = ListDataFragmentDirections.actionGoToDetails()
+            Navigation.findNavController(it).navigate(actionToDetail)
+        }
+    }
+
+}

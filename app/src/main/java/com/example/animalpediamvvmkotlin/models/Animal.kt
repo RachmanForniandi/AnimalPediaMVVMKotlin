fbb3fafec1c47687943d5d8de5c876e064b2ab2c
RachmanForniandi <rachmanforniandi@gmail.com>
2020-06-28 23:16:13 +0700

    preparing model class done.

--- app/src/main/java/com/example/animalpediamvvmkotlin/models/Animal.kt
+++ app/src/main/java/com/example/animalpediamvvmkotlin/models/Animal.kt
@@ -1,6 +1,34 @@
 package com.example.animalpediamvvmkotlin.models
 
+import com.google.gson.annotations.SerializedName
+
+
+data class ApiKey(
+    val message:String?,
+    val key:String?
+)
 data class Animal (
-    val name:String?
+    val name:String?,
+    val taxonomy: Taxonomy?,
+    val location:String?,
+    val speed:String?,
+    val diet: String?,
+
+    @SerializedName("lifespan")
+    val lifeSpan: String?,
+
+    @SerializedName("Image")
+    val imageURL: String?
+)
+
+data class Taxonomy (
+    var kingdom: String?,
+    var order: String?,
+    var family: String?
+)
+
+data class Speed (
+    var metric: String? ,
+    var imperial: String?
 )
 
